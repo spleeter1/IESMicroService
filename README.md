41912489e65c016cda78eb213668da2a87e63666
spleeter <ngdnam03@gmail.com>
2025-04-08 23:13:36 +0700

    initialirz

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # IESMicroService
+
+**Eureka Server**
+
+-   spring-cloud-starter-netflix-eureka-server
+
+**API Gateway**
+
+-   spring-cloud-starter-gateway,
+-   spring-cloud-starter-netflix-eureka-client
+
+**Microservices**
+
+-   spring-boot-starter-web,
+-   spring-cloud-starter-netflix-eureka-client,
+-   spring-boot-starter-data-jpa,
+-   mysql-connector-java,
+-   spring-cloud-starter-openfeign
--- eureka-server/src/main/java/com/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka_server/EurekaServerApplication.java
@@ -1,13 +1,13 @@
-package com.api_gateway;
+package com.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
