494a95a9e588ab66c550126e128061a99cdae7c7
spleeter <ngdnam03@gmail.com>
2025-04-08 16:57:09 +0700

    group actor

--- user-service/src/main/java/com/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/user_service/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.agent_service;
+package com.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgentServiceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AgentServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
--- user-service/src/test/java/com/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/user_service/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.supplier_service;
+package com.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SupplierServiceApplicationTests {
+class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
