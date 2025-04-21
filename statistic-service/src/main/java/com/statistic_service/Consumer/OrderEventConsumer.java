6a1282fa0cd2a21a6339d619691325d0e664af9c
spleeter <ngdnam03@gmail.com>
2025-04-21 22:41:59 +0700

    update some configurations

--- statistic-service/src/main/java/com/statistic_service/Consumer/OrderEventConsumer.java
+++ statistic-service/src/main/java/com/statistic_service/Consumer/OrderEventConsumer.java
@@ -3,7 +3,6 @@ package com.statistic_service.Consumer;
 import com.statistic_service.Event.OrderEventCreated;
 import com.statistic_service.Service.StatisticService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
